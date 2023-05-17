be841f3ac9a52634366e9cd40ed43c80f842bf98
jaymody <jaykmody@gmail.com>
2023-05-17 14:54:47 -0400

    Implement game of life.

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
-import init from "./pkg/hello_world.js";
+import init, { Universe } from "./pkg/cgol_wasm.js";
+
+const FPS = 10;
+const CELL_SIZE = 5; // px
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
 const runWasm = async () => {
-  const helloWorld = await init("./pkg/hello_world_bg.wasm");
-  const addResult = helloWorld.add(24, 24);
-  document.body.textContent = `Hello World! addResult: ${addResult}`;
+  const { memory } = await init("./pkg/cgol_wasm_bg.wasm");
+
+  const universe = Universe.new(64, 64);
+  const width = universe.nrows();
+  const height = universe.ncols();
+
+  const canvas = document.getElementById("canvas");
+  canvas.height = (CELL_SIZE + 1) * height + 1;
+  canvas.width = (CELL_SIZE + 1) * width + 1;
+
+  const ctx = canvas.getContext('2d');
+
+  const getIndex = (row, column) => {
+    return row * width + column;
+  };
+
+  const draw = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col);
+
+        ctx.fillStyle = cells[idx] ? ALIVE_COLOR : DEAD_COLOR;
+        ctx.fillRect(
+          col * (CELL_SIZE + 1) + 1,
+          row * (CELL_SIZE + 1) + 1,
+          CELL_SIZE,
+          CELL_SIZE
+        );
+      }
+    }
+
+    ctx.stroke();
+  }
+
+  const renderLoop = () => {
+    setTimeout(() => {
+      universe.tick();
+      draw();
+      requestAnimationFrame(renderLoop)
+    }, 1000 / FPS);
+  };
+
+  draw();
+  requestAnimationFrame(renderLoop);
 };
 
 runWasm();
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,78 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    return a + b;
+pub struct Universe {
+    nrows: u32,
+    ncols: u32,
+    cells: Vec<bool>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new(nrows: u32, ncols: u32) -> Self {
+        Universe {
+            nrows: nrows,
+            ncols: ncols,
+            cells: (0..nrows * ncols)
+                .map(|i| i % 2 == 0 || i % 7 == 0)
+                .collect(),
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let mut new_cells = self.cells.clone();
+        for row in 0..self.nrows {
+            for col in 0..self.ncols {
+                let idx = self.get_index(row, col);
+                let count = self.num_alive_neighbors(row, col);
+                let is_alive = self.cells[idx];
+                new_cells[idx] = count == 3 || (is_alive && count == 2)
+            }
+        }
+        self.cells = new_cells;
+    }
+
+    pub fn cells(&self) -> *const bool {
+        self.cells.as_ptr()
+    }
+
+    pub fn nrows(&self) -> u32 {
+        self.nrows
+    }
+
+    pub fn ncols(&self) -> u32 {
+        self.ncols
+    }
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        ((self.ncols * row) + col) as usize
+    }
+
+    fn num_alive_neighbors(&self, row: u32, col: u32) -> u32 {
+        let left = if col == 0 { self.ncols - 1 } else { col - 1 };
+        let right = if col >= self.ncols - 1 { 0 } else { col + 1 };
+        let top = if row == 0 { self.nrows - 1 } else { row - 1 };
+        let bottom = if row >= self.nrows - 1 { 0 } else { row + 1 };
+
+        let mut count = 0;
+        for (i, j) in [
+            (top, left),
+            (top, col),
+            (top, right),
+            (row, left),
+            (row, right),
+            (bottom, left),
+            (bottom, col),
+            (bottom, right),
+        ] {
+            if i == row && j == col {
+                continue;
+            }
+            count += self.cells[self.get_index(i, j)] as u32;
+        }
+
+        count
+    }
 }
